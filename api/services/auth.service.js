00e759d2e09ed4f5b298fbeda08c8f37a03ae820
George Padmore Yeboah <padmorey@gmail.com>
2022-02-23 21:55:03 +0000

    debugging authservice

--- api/services/auth.service.js
+++ api/services/auth.service.js
@@ -3,6 +3,7 @@ const jwt = require('jsonwebtoken');
 const secret = process.env.NODE_ENV === 'production' ? process.env.JWT_SECRET : 'thisismysecret';
 
 const authService = () => {
+  console.log(`process.env.NODE_ENV - ${process.env.NODE_ENV}, process.env.JWT_SECRET - ${process.env.JWT_SECRET} , secret = ${secret}`);
   const issue = (payload) => jwt.sign(payload, secret, { expiresIn: 10800 });
   const verify = (token, cb) => jwt.verify(token, secret, {}, cb);
 
